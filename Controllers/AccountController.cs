ee9299e70468c0942651355bf39b941ddd13df2a
GGOShinoAsada <mstr.calling@gmail.com>
2020-09-14 02:11:24 +0300

    Отмена проверки подинности подтверждения адреса электронной почты всвязи с удалением функции подтверждения Email

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -68,44 +68,44 @@ namespace workwithidentity.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
-            if (ModelState.IsValid)
+            //if (ModelState.IsValid)
+            //{
+            //    var user = await UserManager.FindAsync(model.Email, model.Password);
+            //    if (user != null)
+            //    {
+            //        if (user.EmailConfirmed == true)
+            //        {
+            //            await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+            //            return RedirectToLocal(returnUrl);
+            //        }
+            //        else
+            //        {
+            //            ModelState.AddModelError("", "Invalid Login or password");
+            //        }
+            //    }
+            //}
+            //return View(model);
+            if (!ModelState.IsValid)
             {
-                var user = await UserManager.FindAsync(model.Email, model.Password);
-                if (user != null)
-                {
-                    if (user.EmailConfirmed == true)
-                    {
-                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                        return RedirectToLocal(returnUrl);
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("", "Invalid Login or password");
-                    }
-                }
+                return View(model);
             }
-            return View(model);
-            //if (!ModelState.IsValid)
-            //{
-            //    return View(model);
-            //}
 
-            //// Сбои при входе не приводят к блокированию учетной записи
-            //// Чтобы ошибки при вводе пароля инициировали блокирование учетной записи, замените на shouldLockout: true
-            //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-            //switch (result)
-            //{
-            //    case SignInStatus.Success:
-            //        return RedirectToLocal(returnUrl);
-            //    case SignInStatus.LockedOut:
-            //        return View("Lockout");
-            //    case SignInStatus.RequiresVerification:
-            //        return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-            //    case SignInStatus.Failure:
-            //    default:
-            //        ModelState.AddModelError("", "Неудачная попытка входа.");
-            //        return View(model);
-            //}
+            // Сбои при входе не приводят к блокированию учетной записи
+            // Чтобы ошибки при вводе пароля инициировали блокирование учетной записи, замените на shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Неудачная попытка входа.");
+                    return View(model);
+            }
         }
 
 
