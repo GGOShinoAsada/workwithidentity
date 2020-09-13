cd1100031d7b0e4f5b828344ae20b82c77d7ca8e
GGOShinoAsada <mstr.calling@gmail.com>
2020-09-13 19:55:06 +0300

    Продемострирован механизм переопределения пользователей

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -3,12 +3,18 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace workwithidentity.Models
 {
     // В профиль пользователя можно добавить дополнительные данные, если указать больше свойств для класса ApplicationUser. Подробности см. на странице https://go.microsoft.com/fwlink/?LinkID=317594.
     public class ApplicationUser : IdentityUser
     {
+        public List<Good> Goods { get; set; }
+        public ApplicationUser()
+        {
+            Goods = new List<Good>();
+        }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
@@ -24,6 +30,8 @@ namespace workwithidentity.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public DbSet<Good> Goods { get; set; }
 
         public static ApplicationDbContext Create()
         {
